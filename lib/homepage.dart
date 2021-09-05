e99e5a2e00c2140f34a7407584f899f08dfc101e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-05 23:15:19 +0700

    update homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,8 +11,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Home Page"),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          "Home Page",
+          style: GoogleFonts.poppins(
+            color: Colors.purple[700],
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              //
+            },
+            icon: Icon(
+              CupertinoIcons.bell_fill,
+              color: Colors.purple[700],
+            ),
+          ),
+        ],
       ),
       body: Container(
         child: Column(
@@ -19,6 +42,65 @@ class _HomePageState extends State<HomePage> {
             Text("Test"),
             Text("Test"),
           ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purpleAccent[400],
+        onPressed: () {
+          //
+        },
+        child: Icon(
+          CupertinoIcons.add_circled_solid,
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          decoration: BoxDecoration(
+            color: Colors.deepPurple[700],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {
+                  //
+                },
+                icon: Icon(
+                  CupertinoIcons.house_fill,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  //
+                },
+                icon: Icon(
+                  CupertinoIcons.bag_fill,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  //
+                },
+                icon: Icon(
+                  CupertinoIcons.shopping_cart,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  //
+                },
+                icon: Icon(
+                  CupertinoIcons.person_fill,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
