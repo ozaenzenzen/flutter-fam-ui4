8769df05baaeec7e338106c0254ec32631935e36
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-05 23:53:24 +0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fam_ui4/homepage.dart';
+import 'package:flutter_fam_ui4/mainpage.dart';
 import 'package:get/get.dart';
 
 void main() {
